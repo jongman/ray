6525d830a78f7f242b558232dc97a0695c293175
JongMan Koo <jongman@gmail.com>
2013-01-09 17:36:13 -0600

    test screen in png

--- cpp/ray.cpp
+++ cpp/ray.cpp
@@ -12,12 +12,17 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include"lodepng.h"
 using namespace std;
 int main() {
-  int cases;
-  cin >> cases;
-  for(int cc = 0; cc < cases; ++cc) {
-
-  }
+  vector<unsigned char> scene(640*480*4);
+  for(int y = 0; y < 480; ++y)
+    for(int x = 0; x < 640; ++x) {
+      if((y / 50) % 2 == 0) {
+        scene[(y*640+x)*4+1] = (y+x) % 255;
+        scene[(y*640+x)*4+3] = 255;
+      }
+    }
+  lodepng::encode("hello.png", scene, 640, 480);
 }
 
